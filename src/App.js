86a5fd67e76682272888ba5f5fef12055929a1af
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-03 17:01:49 +0530

    Assignment Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    
+    <div>
+      <h2>Mobile Operating System</h2>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h2>Mobile Manufacturers</h2>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
   );
 }
 
